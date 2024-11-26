80c6942c17522193b69f929c30603befe81a6441
adrianweidig <adrianweidig@gmx.net>
2024-11-26 09:02:39 +0100

    Übungserklärung

--- Uebungen/pospar.sh
+++ Uebungen/pospar.sh
@@ -1,8 +1,22 @@
 #!/bin/sh
 
-echo "Es wurden $# PosParameter übergeben"
+# 1. Zeige die Anzahl der übergebenen Positionsparameter an
+printf "Es wurden %d PosParameter übergeben\n" "$#"
 
-for item in $(seq 0 $#); do
-    eval wert=\${"$item"}
-    echo "$item ist : $wert"
+# 2. Überprüfe, ob Parameter übergeben wurden
+if [ $# -eq 0 ]; then
+    printf "Es wurden keine Parameter übergeben.\n"
+    exit 1
+fi
+
+# 3. Schleife über alle übergebenen Parameter
+# Wir starten bei 0, weil $0 der Name des Skripts ist und wir auch diesen sehen wollen
+for i in $(seq 0 $#); do
+    # 4. Zugriff auf den i-ten Parameter mit ${!i} (indirekte Referenzierung)
+    parameter_indirect=${!i}
+    printf "Parameter %d mit indirekter Referenzierung (\${!i}): %s\n" "$i" "$parameter_indirect"
+
+    # 5. Zugriff auf den i-ten Parameter mit eval und \$$i (Alternative Methode) und geschweiften Klammern für Parameter >9
+    eval parameter_eval=\${"$i"}
+    printf "Parameter %d mit eval und \${%d}: %s\n" "$i" "$i" "$parameter_eval"
 done

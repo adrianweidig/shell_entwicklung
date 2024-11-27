744f0b40e4aaa6c3ea4248f5fd5d6661a3cfae44
adrianweidig <adrianweidig@gmx.net>
2024-11-27 15:12:28 +0100

    Anpassung Uebung Generate Users

--- Uebungen/generate_users.sh
+++ Uebungen/generate_users.sh
@@ -29,11 +29,17 @@ yad --title="Drag and Drop mit YAD" \
             # mit -s Shell
             echo "useradd -m -u "$start_uid" -s /usr/bin/bash -p "$initial_password" "$line""
 
+            # Passwort muss beim nächsten Anmelden geändert werden
+            echo "passwd -e "$line""
+
+            # Alternativ kann einfach die /home/skel/.bashrc angepasst werden da diese dann direkt
+            # bei dem neuen Nutzer erstellt wird
+
             # Kopiert vom Nutzer kit die .bashrc in das Homeverzeichnis des neuen Nutzers
-            echo "cp /home/kit/.bashrc /home/"$line"/.bashrc"
+            # echo "cp /home/kit/.bashrc /home/"$line"/.bashrc"
 
             # Ändert die Besitzrechte der .bashrc auf die des neuen Nutzers
-            echo "chown "$line":"$line" /home/"$line"/.bashrc"
+            #echo "chown "$line":"$line" /home/"$line"/.bashrc"
 
             start_uid=$((start_uid + 1))
         done < "$FILE_PATH" 

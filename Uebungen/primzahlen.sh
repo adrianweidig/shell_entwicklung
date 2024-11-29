e7a4237cffd9acb107eaa2f21cfc5599560dfa4f
adrianweidig <adrianweidig@gmx.net>
2024-11-29 09:05:29 +0100

    Anpassung Uebung Primzahl Beschreibung

--- Uebungen/primzahlen.sh
+++ Uebungen/primzahlen.sh
@@ -6,24 +6,24 @@ if [ $# -eq 0 ]; then
 fi
 
 
-for ((i = 2; i <= $1; i++)); do
-    is_prime=1
-    for ((j = 2; j < $i; j++)); do
-        if [ $((i % j)) -eq 0 ]; then
-            is_prime=0
+for ((zu_pruefende_zahl = 2; zu_pruefende_zahl <= $1; zu_pruefende_zahl++)); do
+    ist_primzahl=1
+    for ((linke_grenze = 2; linke_grenze < zu_pruefende_zahl; linke_grenze++)); do
+        if [ $((zu_pruefende_zahl % linke_grenze)) -eq 0 ]; then
+            ist_primzahl=0
             break
         fi
     done
-    if [ $is_prime -eq 1 ]; then
-        echo $i
+    if [ $ist_primzahl -eq 1 ]; then
+        echo $zu_pruefende_zahl
     fi
 done    
 
 # Vorgehensweise des Skriptes:
 # 1. Schleife, die von 2 bis dem ersten Parameter iteriert wird.
 #    --> Dies wird gemacht da die kleinste Primzahl bei 2 beginnt
-# 2. Innerer Schleife, die von 2 bis dem aktuellen Wert (j) iteriert wird.
-#    um zu prüfen ob j ein Teiler von i ist.
-# 3. Wenn j ein Teiler von i ist, ist es keine Primzahl da ein weiterer Teiler
+# 2. Innerer Schleife, die von 2 als linke_grenze bis zu_pruefende_zahl iteriert wird.
+#    um zu prüfen ob linke_grenze ein Teiler von zu_pruefende_zahl ist.
+# 3. Wenn linke_grenze ein Teiler von zu_pruefende_zahl ist, ist es keine Primzahl da ein weiterer Teiler
 #    gefunden wurde.    
-# 4. Wenn keine Teiler gefunden wurden ist i eine Primzahl
+# 4. Wenn keine Teiler gefunden wurden ist zu_pruefende_zahl eine Primzahl
